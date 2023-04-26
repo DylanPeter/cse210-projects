3e3c85eb94d6e7d930facda2c3e9238372d887c6
Dylan Petersen <pete1048310@byui.edu>
2023-04-26 10:58:39 -0600

    Made a grade to letter calculator in C#

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,50 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string percentage = Console.ReadLine();
+        int percent = int.Parse(percentage);
+        
+        string letter = "";
+
+        if (percent >= 90)
+        {
+            // Console.WriteLine("Congrats! Your grade is an A! ");
+            letter = "A";
+        }
+        
+        else if (percent >= 80)
+        {
+            // Console.WriteLine("Congrats! Your grade is an B! ");
+            letter = "B";
+        }
+        
+        else if (percent >= 70)
+        {
+            // Console.WriteLine("Your grade is a C");
+            letter = "C";
+        }
+
+        else if (percent >= 60)
+        {
+            // Console.WriteLine("Your grade is a D :( ");
+            letter = "D";
+        }
+        
+        else
+        {
+            // Console.WriteLine("Unfortunately, you got an F :( ");
+            letter = "F";
+        }
+        Console.WriteLine($"Your letter grade is {letter}.");
+
+        if (percent >= 70)
+        {
+            Console.WriteLine("Congrats! You passed the class!");
+        }
+        else
+        {
+            Console.WriteLine("Unfortunetly, you didn't pass the class :(. Make sure to study harder next time!");
+        }
     }
 }
