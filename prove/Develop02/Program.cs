c94b7b342615606f979cf9abd4c38140ec7a0d54
Dylan Petersen <pete1048310@byui.edu>
2023-05-08 11:14:03 -0600

    Started writing a program about a journal

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -1,9 +1,33 @@
+using System.Collections.Generic;
 using System;
-
-class Program
+namespace Develop02
 {
-    static void Main(string[] args)
+    public class Progam
     {
-        Console.WriteLine("Hello Develop02 World!");
+        static void Main(string[] args)
+        {
+            Entry myEntry = new Entry();
+            myEntry.StorePrompt("What is your name?");
+            myEntry.StoreResponse("Dylan Petersen");
+            myEntry.StoreDate("May 8 2023");
+
+            Entry yourEntry = new Entry();
+            yourEntry.StorePrompt("What is your favorite food?");
+            yourEntry.StoreResponse("Pizza");
+            yourEntry.StoreDate("8 May 2023");
+
+
+            Journal journal = new Journal();
+            journal.StoreEntry(myEntry);
+            journal.StoreEntry(yourEntry);
+
+            List<Entry> entries = journal.GetEntries();
+            foreach (Entry entry in entries)
+            {
+                string message 
+                    = $"{entry.GetDate()}\n{entry.GetPrompt()}\n{entry.GetResponse()}\n";
+                Console.WriteLine(message);
+            }
+        }
     }
-}
+}  
