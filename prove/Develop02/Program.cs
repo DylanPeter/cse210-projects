3a367443b17200f8c6eb96f9acb5f8839108a757
Dylan Petersen <pete1048310@byui.edu>
2023-05-22 11:03:27 -0600

    Created a journal program in c#

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -1,33 +1,127 @@
-using System.Collections.Generic;
 using System;
-namespace Develop02
+using System.IO;
+
+class Program
 {
-    public class Progam
+
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        int[] validNumbers = { 1, 2, 3, 4, 5 };
+        int action = 0;
+        Console.Write("\n**** Welcome to the Journal App ****\n");
+        // Create new journal reference/list
+        Journal journal = new Journal();
+        JournalPrompt jp = new JournalPrompt();
+
+        while (action != 5)
         {
-            Entry myEntry = new Entry();
-            myEntry.StorePrompt("What is your name?");
-            myEntry.StoreResponse("Dylan Petersen");
-            myEntry.StoreDate("May 8 2023");
+            // Ask for user input (1-5)
+            //Call Choices
+            action = Choices();
 
-            Entry yourEntry = new Entry();
-            yourEntry.StorePrompt("What is your favorite food?");
-            yourEntry.StoreResponse("Pizza");
-            yourEntry.StoreDate("8 May 2023");
-
-
-            Journal journal = new Journal();
-            journal.StoreEntry(myEntry);
-            journal.StoreEntry(yourEntry);
-
-            List<Entry> entries = journal.GetEntries();
-            foreach (Entry entry in entries)
+            switch (action)
             {
-                string message 
-                    = $"{entry.GetDate()}\n{entry.GetPrompt()}\n{entry.GetResponse()}\n";
-                Console.WriteLine(message);
+                case 1:
+                    // Write Journal Entry
+                    string entryId = GetEntryId();
+                    string dateInfo = GetDateTime();
+                    string prompt = jp.GetPrompt();
+
+                    JournalEntry entry = new JournalEntry();
+                    entry._entryNumber = entryId;
+                    entry._dateTime = dateInfo;
+                    entry._journalPrompt = prompt;
+
+                    Console.Write($"{prompt}\n");
+                    Console.Write(">>> ");
+                    string userEntry = Console.ReadLine();
+                    entry._journalEntry = userEntry;
+
+                    journal._journal.Add(entry);
+
+                    // Console.WriteLine("Success Choice 1!");
+                    break;
+                case 2:
+                    // Display Journal Entries
+                    journal.Display();
+                    // Console.WriteLine("Success Choice 2!");
+                    break;
+                case 3:
+                    // Load text file
+                    journal.LoadJournalFile();
+                    // Console.WriteLine("Success Choice 3!");
+                    break;
+                case 4:
+                    // Save to text file
+                    journal.CreateJournalFile();
+                    // Console.WriteLine("Success Choice 4!");
+                    break;
+                case 5:
+                    // Quite
+                    Console.WriteLine("\nThank you for using the Journal App!\n");
+                    break;
+                default:
+                    Console.WriteLine($"\nSorry the option you entered is not valid.");
+                    break;
             }
         }
     }
-}  
+
+    static int Choices()
+    // Method to display choices to user
+    {
+        string choices = @"
+Please select one of the following choices:
+1. Write
+2. Display
+3. Load
+4. Save
+5. Quit
+What would you like to do? ";
+
+        Console.Write(choices);
+        string userInput = Console.ReadLine();
+        int action = 0;
+        // This block catches any non integer values that are entered
+        try
+        {
+            action = int.Parse(userInput);
+        }
+        catch (FormatException)
+        {
+            action = 0;
+        }
+        catch (Exception exception)
+        {
+            Console.WriteLine(
+                $"Unexpected error:  {exception.Message}");
+        }
+        return action;
+    }
+
+    static string GetDateTime()
+    // Method to get the date and time for journal record
+    {
+        DateTime now = DateTime.Now;
+        string currentDateTime = now.ToString("F");
+        // Console.WriteLine(now.ToString("F"));
+        // Displays Saturday, March 01, 2008 7:00:00 AM
+        return currentDateTime;
+    }
+    static void AddJournalEntry()
+    // Method to add entry to text file
+    {
+        string MyJournalFile = "MyJournal.txt";
+        File.AppendAllText(MyJournalFile, "");
+    }
+
+    static string GetEntryId()
+    {
+        Guid entryuuid = Guid.NewGuid();
+        string entryuuidAsString = entryuuid.ToString();
+
+        return entryuuidAsString;
+    }
+
+
+} 
---
+++ prove/Develop03/Reference.cs
@@ -0,0 +1,8 @@
+using System.Collections.Generic;
+namespace Develop03
+{
+    public class Reference 
+    {
+        
+    }
+}
